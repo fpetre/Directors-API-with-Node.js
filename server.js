43e3f79e6d793a753cad7a056f9f6cc7f83b2245
App Academy Student <appacademy@Apps-Mac-mini-10.local>
2015-01-07 14:23:01 -0500

    set up post route to create a Director

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
 
-//configure app to use bodyParser()
+// configure app to use bodyParser()
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-//configure mongoose to use monogDB modulus database
+// configure mongoose to use monogDB modulus database
 var mongoose   = require('mongoose');
 mongoose.connect('mongodb://directorApi:directorApi@proximus.modulusmongo.net:27017/Pyzote2t')
 
@@ -25,11 +25,41 @@ var port = process.env.PORT || 3000;
 // ==========================================================
 
 var router = express.Router();
+
+// midleware for all requests
+router.use(function(req, res, next) {
+  // log request
+  console.log('Something is happening.');
+  next();
+});
+
 router.get('/', function(req, res){
-  res.json({message: 'hooray! welcome to our api!' });
+  res.json({ message: 'hooray! welcome to our api!' });
 });
 
 // more routes for our API will happen here
+//routes that end in /bears
+
+router.route('/directors')
+  // create a director
+  .post(function(req, res){
+
+    var director = new Director();
+    director.full_name = req.body.full_name;
+    director.dob = req.body.dob;
+    director.favorite_camera = req.body.favorite_camera;
+    director.favorite_movies = req.body.favorite_movies;
+
+    director.save(function(err) {
+      if (err) {
+        res.send(err);
+      }
+      res.json({ message: 'Director created!' });
+    });
+
+  });
+
+
 
 // REGISTER ROUTES
 // all of our routes will be prefixed with /api
